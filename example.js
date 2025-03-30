da63d3ab7574e4d0a3ffd033c1c3cf7e31ed9a58
coderxi1 <coderxi@qq.com>
2025-03-30 13:40:47 +0800

    feat: update dependencies and add template for HTML output

--- example.js
+++ example.js
@@ -3,8 +3,10 @@ import { remark } from 'remark'
 import remarkMarkmap from 'remark-markmap'
 import fs from 'fs'
 
+const VERSION = JSON.parse(fs.readFileSync('package.json')).dependencies['remark-markmap'].slice(1)
+
 const file = await remark()
   .use(remarkMarkmap)
   .process(await read('example.md'))
 
-fs.writeFileSync("example.html", String(file))
+fs.writeFileSync("index.html", new Function(['slot','version'], `return \`${fs.readFileSync('template.html')}\`;`)(String(file),VERSION))
